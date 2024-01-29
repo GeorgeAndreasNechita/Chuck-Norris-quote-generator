056a05c6f8014953b31a12f58c47218965231a70
Andreas George Nechita <ga.nechita@gmail.com>
2024-01-29 19:45:24 +0100

    added picture of Chuck Norris

--- pages/index.js
+++ pages/index.js
@@ -33,14 +33,13 @@ export default function Home() {
       </Head>
 
       <main>
-
         <img src="/W&S.png" alt="Vercel" className="flex justify-start" />
         <div className="grid grid-cols-2 gap-4">
-          <div className="p-4 border grid justify-center items-center">Image</div>
+          <div className="p-4 border grid justify-center items-center">        <img src="https://www.martin-missfeldt.de/images-pictures/time-lapsed-videos/cool-chuck-norris-maschinenpistole-jeans.jpg" alt="Vercel" className="flex justify-start" /></div>
           <div className="p-4 border grid justify-center items-center">
             <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-6 mb-0' onClick={getQuote}> New Quote</button>
             <div className='m-6 font-bold text-center'>{quote}</div>
-            </div>
+          </div>
           <div>
           </div>
         </div>
