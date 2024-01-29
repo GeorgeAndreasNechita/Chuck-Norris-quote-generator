04196997603120f9c0ab714301c7f75b0561557b
Andreas George Nechita <ga.nechita@gmail.com>
2024-01-29 21:30:36 +0100

    improved responsiveness

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
   return (
 
-    <div className="my-16 mx-64 px-4 border-2 border-green-300 rounded-3xl">
+    <div className="my-16 sm:mx-6 md:mx-32 lg:mx-64 xl:mx-64 2xl:mx-96 px-4 border-2 border-green-300 rounded-3xl">
       <Head>
         <title>W&S - Probearbeit - Andreas George Nechita</title>
         <link rel="icon" href="/favicon.ico" />
@@ -38,9 +38,9 @@ export default function Home() {
 
       <main>
         {/* W&S logo */}
-        <img src="/W&S.png" alt="W&S logo" className="flex justify-start w-36" />
+        <img src="/W&S.png" alt="W&S logo" className="flex justify-start w-32 sm:w-36 md:w-36" />
 
-        <div className="grid grid-cols-9 gap-4 my-4">
+        <div className="grid grid-cols-9 md:gap-4 my-4 sm:mx-16">
           {/* Col-1 Picture of Chuck Norris */}
           <div className="grid justify-center items-center col-span-4">
             <img src="https://www.knoxnews.com/gcdn/presto/2018/11/08/USAT/6eeba0d5-e387-4cd1-80bc-740dd3586b07-XXX_E_COMING_ATTRACTIONS_06_WALKER.JPG" className="h-80 flex justify-start" />
@@ -48,8 +48,8 @@ export default function Home() {
 
           {/* Col-2 Quote button + Quote  */}
           <div className="grid justify-center items-center col-span-5">
-            <button className='w-64 mb-0 py-2 px-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white  border border-blue-500 hover:border-transparent rounded  justify-self-center' onClick={getQuote}> New Quote</button>
-            <div className='m-6 mx-16 font-bold text-center'>{quote}</div>
+            <button className='sm:w-32 md:w-40 mb-0 py-2 px-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white  border border-blue-500 hover:border-transparent rounded  justify-self-center' onClick={getQuote}> New Quote</button>
+            <div className='m-4 sm:mx-8 md:mx-10 font-bold text-center'>{quote}</div>
           </div>
         </div>
       </main>
