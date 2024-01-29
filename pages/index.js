bb9fc725951b3064fcb3e00dc42cae044b34f78e
Andreas George Nechita <ga.nechita@gmail.com>
2024-01-29 18:55:01 +0100

    removed redundant file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React, { useState, useEffect } from "react";
-import Message from './Message';
+
 
 export default function Home() {
 
