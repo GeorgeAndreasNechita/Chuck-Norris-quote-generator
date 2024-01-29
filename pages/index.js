37b08711536e4f908904b9b4439d9835e50ad46e
Andreas George Nechita <ga.nechita@gmail.com>
2024-01-29 20:32:24 +0100

    some stile changes

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       .then((data) => setVisitorCount(data.count))
       .catch((error) => console.error('Error fetching visitor count:', error));
 
-      getQuote();
+    getQuote();
   }, []);
 
   function getQuote() {
@@ -30,16 +30,19 @@ export default function Home() {
 
   return (
 
-    <div className="m-16 border-2 border-black">
+    <div className="my-16 mx-64 p-8 border-2 border-black">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <img src="/W&S.png" alt="Vercel" className="flex justify-start" />
+        <img src="/W&S.png" alt="Vercel" className="flex justify-start w-40" />
         <div className="grid grid-cols-2 gap-4">
-          <div className="p-4 border grid justify-center items-center">        <img src="https://www.martin-missfeldt.de/images-pictures/time-lapsed-videos/cool-chuck-norris-maschinenpistole-jeans.jpg" alt="Vercel" className="flex justify-start" /></div>
+
+          <div className="p-4 border grid justify-center items-center">
+             <img src="https://www.martin-missfeldt.de/images-pictures/time-lapsed-videos/cool-chuck-norris-maschinenpistole-jeans.jpg" alt="Vercel" className="flex justify-start" />
+          </div>
           <div className="p-4 border grid justify-center items-center">
             <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded  mb-0 w-64 justify-self-center' onClick={getQuote}> New Quote</button>
             <div className='m-6 font-bold text-center'>{quote}</div>
@@ -52,7 +55,7 @@ export default function Home() {
       </main>
 
       <footer>
-      <div className='grid justify-center items-center my-12 font-bold'>Visitor Count: {visitorCount}</div>
+        <div className='grid justify-center items-center my-12 font-bold'>Visitor Count: {visitorCount}</div>
       </footer>
 
       <style jsx>{`
