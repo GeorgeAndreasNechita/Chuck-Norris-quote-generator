c7f95ff463f2737b4fcd78cd49dfff95bcfbbbbc
Andreas George Nechita <ga.nechita@gmail.com>
2024-01-29 18:53:58 +0100

    changed the quote functionality so that it shows a new one only when you press the button

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React, { useState, useEffect } from "react";
+import Message from './Message';
 
 export default function Home() {
 
   const [quote, setQuote] = useState(null);
 
-  useEffect(() => {
-    fetch("https://api.chucknorris.io/jokes/random?category=dev", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setQuote(data.value);
-        console.log(data.value);
+  function getQuote(){
+      fetch("https://api.chucknorris.io/jokes/random?category=dev", {
+        method: "GET",
       })
-      .catch((error) => console.log(error));
-  }, []);
+        .then((response) => response.json())
+        .then((data) => {
+          setQuote(data.value);
+          console.log(data.value);
+        })
+        .catch((error) => console.log(error));
+  }
+
+
 
   return (
 
@@ -29,11 +32,10 @@ export default function Home() {
       <main>
 
         <img src="/W&S.png" alt="Vercel" className="flex justify-start" />
-
         <div className="grid grid-cols-2 gap-4">
           <div className="p-4 border flex justify-center items-center">Image</div>
           <div className="p-4 border flex justify-center items-center">
-            <button > New Quote</button>
+            <button onClick={getQuote}> New Quote</button>
             {quote}</div>
           <div>
           </div>
